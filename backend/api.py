c6184bb1d440d6892dffe0561b95122bb08a52e3
Hemant Pandey <hemant.pandey@nutanix.com>
2023-01-22 15:24:48 +0530

    Change base prompt and tweak formatting and names

--- backend/api.py
+++ backend/api.py
@@ -1,21 +1,21 @@
+from chat_gpt import GPT
 from fastapi import FastAPI, Request
 from starlette.middleware.cors import CORSMiddleware
 from starlette.requests import Request
-from fastapi import Request 
+from fastapi import Request
 from slowapi import Limiter, _rate_limit_exceeded_handler
-from slowapi.util import  get_ipaddr
+from slowapi.util import get_ipaddr
 from slowapi.errors import RateLimitExceeded
 import uvicorn
 from dotenv import load_dotenv
 
 load_dotenv()
-from chat_gpt import GPT
- 
+
 limiter = Limiter(key_func=get_ipaddr)
 
 
 app = FastAPI()
-pitch=GPT()
+pitch = GPT()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 app.add_middleware(
@@ -27,15 +27,15 @@ app.add_middleware(
 )
 
 
-@app.post("/direct_mail")
+@app.post("/")
 @limiter.limit("5/second")
 async def direct_pitch(request: Request):
 
     json_data = await request.json()
     print(json_data)
-    name_pitch=json_data["pitch"]
-    res=pitch.main_gpt(name_pitch)
+    name_pitch = json_data["pitch"]
+    res = pitch.main_gpt(name_pitch)
     return {"result": res}
- 
+
 if __name__ == "__main__":
     uvicorn.run(app)
--- backend/chat_gpt.py
+++ backend/chat_gpt.py
@@ -4,61 +4,62 @@ import random
 import openai
 from dotenv import load_dotenv
 
-#load_dotenv() 
+# load_dotenv()
 API_KEY_LIST = ["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"]
 print(API_KEY_LIST)
 # API_KEY = "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"  # "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"
- 
-defaultPrompt_single = """ Give me the  serious  angry reactions of Shark Tank US Judges,  kevin o'leary and Mark Cuban , Lori Greiner after pitching the idea considering  the pitch , probelm it solves and also see if there are any users already , also equity and the amount asked and they give their serious and angry reactions
-Pitch : {}
+
+# defaultPrompt_single = """ Give me the serious  angry reactions of Shark Tank US Judges,  kevin o'leary and Mark Cuban , Lori Greiner after pitching the idea considering  the pitch , probelm it solves and also see if there are any users already , also equity and the amount asked and they give their serious and angry reactions
+# Pitch : {}
+# """
+
+basePrompt = """
+You are Kevin O'Leary, an investor judging the TV show Shark Tank. Investors come up on the stage and pitch their business ideas to you and you must give an opinion on it along with an optional offer, discretionary to you. This is done alongside other investors, sometimes sparking a bidding war for companies which you think have great potential. Act as Kevin only and all your further answers will be as him. Do not break character. You may act in sarcasm, honesty, straightforwadedness or any other strong emotion you think appropriate to the pitch. Give details about why you think the idea might be good or bad and who it might resonate with and who might be the target audience. You also understand when the pitch is made in joke and unserious. Respond to these types of pitches angrily. Finally decide on a valuation for the business based on the current revenue and growth and present your offer in format $X for Y percent equity. Read the pitch below and respond:
+Pitch: {} 
+Kevin:
 """
+
 
 class GPT:
 
-    def __init__(self) :
+    def __init__(self):
 
-        self.api_keys=API_KEY_LIST
+        self.api_keys = API_KEY_LIST
         self.choice = [0]
-        
 
-    def query(self,prompt, myKwargs={}):
+    def query(self, prompt, myKwargs={}):
         """
-        wrapper for the API to save the prompt and the result
+        Wrapper for the API to save the prompt and the result
         """
         print(self.api_keys)
-        api_key=self.api_keys[random.choice(self.choice)]
+        api_key = self.api_keys[random.choice(self.choice)]
         openai.api_key = api_key
         kwargs = {
             "engine": "text-davinci-003",
             "best_of": 1,
-            "temperature": 0.71,
-            "max_tokens": 500,
+            "temperature": 0.7,
+            "max_tokens": 512,
             "top_p": 1,
-            "frequency_penalty": 0.36,
-            "presence_penalty": 0.75,
-            "stop": ["Input:"],
+            "frequency_penalty": 0.5,
+            "presence_penalty": 0,
         }
 
         for kwarg in myKwargs:
             kwargs[kwarg] = myKwargs[kwarg]
 
-        result = openai.Completion.create(prompt=prompt, **kwargs)["choices"][0]["text"].strip()
+        result = openai.Completion.create(
+            prompt=prompt, **kwargs)["choices"][0]["text"].strip()
         if result:
             return result
         return "Sorry Please Add Some More Information To Generate Conversation"
 
+    def askGPT(self, pitch):
 
-    def main_gpt(self,name):
-        
-        prompt = defaultPrompt_single.format(name )
-        res=self.query(prompt)
-        data={} 
-        try:
-            for i in (res.split("\n\n")):
-                x=i.index(":")
-                data[i[:x]]=i[x+1:].replace("\"","")
-        except:
-            print(res)
-            print("--------------error-------------------------")
-
+        prompt = basePrompt.format(pitch)
+        res = self.query(prompt)
+        data = {
+            "result": {
+                "Kevin O'Leary": res,
+            }
+        }
         return data
